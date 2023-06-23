f42fac4d340abc81f2fbdd7e7e97eff9e4b679b5
wuz1mu <kingaal93@gmail.com>
2023-06-23 19:40:07 +0700

    Add typeorm

--- backend/predictor-gateway/src/app.module.ts
+++ backend/predictor-gateway/src/app.module.ts
@@ -3,7 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ImportModule, BuildingObjectModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'buidling_predictor',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
