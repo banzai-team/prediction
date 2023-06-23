3726c96c086070e627b432c4b0569978b2d5dcfb
chOlya <Chvyrova.o@mail.ru>
2023-06-23 21:34:42 +0300

    remove table header, styles

--- dashboard/src/components/DashboardTable.tsx
+++ dashboard/src/components/DashboardTable.tsx
@@ -6,38 +6,34 @@ import {
     useReactTable
 } from '@tanstack/react-table';
 
+import {Table, TableContainer, Thead, Tr, Th, Td, Tbody, Box} from '@chakra-ui/react';
+
 const DashboardTable: React.FC = () => {
     const columns = React.useMemo<ColumnDef<any>[]>(
         () => [
             {
-                header: 'Table',
+                accessorKey: 'objectId',
+                header: 'Object Id',
+                cell: info => info.getValue(),
                 footer: props => props.column.id,
-                columns: [
-                    {
-                        accessorKey: 'objectId',
-                        header: 'Object Id',
-                        cell: info => info.getValue(),
-                        footer: props => props.column.id,
-                    },
-                    {
-                        accessorKey: 'time',
-                        header: 'Time',
-                        cell: info => info.getValue(),
-                        footer: props => props.column.id,
-                    },
-                    {
-                        accessorKey: 'taskCount',
-                        header: 'Task Count',
-                        cell: info => info.getValue(),
-                        footer: props => props.column.id,
-                    },
-                    {
-                        accessorKey: 'progress',
-                        header: 'Progress',
-                        cell: info => info.getValue(),
-                        footer: props => props.column.id,
-                    },
-                ],
+            },
+            {
+                accessorKey: 'time',
+                header: 'Time',
+                cell: info => info.getValue(),
+                footer: props => props.column.id,
+            },
+            {
+                accessorKey: 'taskCount',
+                header: 'Task Count',
+                cell: info => info.getValue(),
+                footer: props => props.column.id,
+            },
+            {
+                accessorKey: 'progress',
+                header: 'Progress',
+                cell: info => info.getValue(),
+                footer: props => props.column.id,
             },
         ], []);
 
@@ -82,51 +78,50 @@ const DashboardTable: React.FC = () => {
         debugTable: true,
     });
 
-    return (
-        <div className="p-2">
-            <div className="h-2"/>
-            <table>
-                <thead>
-                {table.getHeaderGroups().map(headerGroup => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map(header => {
-                            return (
-                                <th key={header.id} colSpan={header.colSpan}>
-                                    {header.isPlaceholder ? null : (
-                                        <div>
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                        </div>
-                                    )}
-                                </th>
-                            )
-                        })}
-                    </tr>
-                ))}
-                </thead>
-                <tbody>
-                {table.getRowModel().rows.map(row => {
-                    return (
-                        <tr key={row.id}>
-                            {row.getVisibleCells().map(cell => {
+    return <Box w="90vw" margin="30px auto">
+        <TableContainer>
+            <Table variant='striped' colorScheme='gray'>
+                <Thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <Tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => {
                                 return (
-                                    <td key={cell.id}>
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext()
+                                    <Th key={header.id} colSpan={header.colSpan}>
+                                        {header.isPlaceholder ? null : (
+                                            <div>
+                                                {flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                            </div>
                                         )}
-                                    </td>
+                                    </Th>
                                 )
                             })}
-                        </tr>
-                    )
-                })}
-                </tbody>
-            </table>
-        </div>
-    );
+                        </Tr>
+                    ))}
+                </Thead>
+                <Tbody>
+                    {table.getRowModel().rows.map(row => {
+                        return (
+                            <Tr key={row.id}>
+                                {row.getVisibleCells().map(cell => {
+                                    return (
+                                        <Td key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </Td>
+                                    )
+                                })}
+                            </Tr>
+                        )
+                    })}
+                </Tbody>
+            </Table>
+        </TableContainer>
+    </Box>;
 };
 
 export default DashboardTable;
