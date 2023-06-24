6934dec6b3959972612b73bda1f420e6bdadf6a9
chOlya <Chvyrova.o@mail.ru>
2023-06-24 15:01:27 +0300

    add margin top

--- dashboard/src/components/GanttChart.tsx
+++ dashboard/src/components/GanttChart.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {FrappeGantt, ViewMode} from 'frappe-gantt-react';
 
+import {Box} from '@chakra-ui/react';
+
 const GanttChart: React.FC<{tasks: any[]}> = ({tasks}) => {
-    return <FrappeGantt
-        tasks={tasks}
-        viewMode={ViewMode.Month}
-        onClick={(task) => console.log(task)}
-        onDateChange={(task, start, end) => console.log(task, start, end)}
-        onProgressChange={(task, progress) => console.log(task, progress)}
-        onTasksChange={(tasks) => console.log(tasks)}
-    />
+    return <Box mt="20px">
+        <FrappeGantt
+            tasks={tasks}
+            viewMode={ViewMode.Month}
+            onClick={(task) => console.log(task)}
+            onDateChange={(task, start, end) => console.log(task, start, end)}
+            onProgressChange={(task, progress) => console.log(task, progress)}
+            onTasksChange={(tasks) => console.log(tasks)}
+        />
+    </Box>;
 };
 
 export default GanttChart;
