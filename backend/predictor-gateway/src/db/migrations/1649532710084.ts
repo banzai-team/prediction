42b325d0fb384c06975fd2f95ab71f0b4f962798
wuz1mu <kingaal93@gmail.com>
2023-06-25 05:49:20 +0700

    Add migration

---
+++ backend/predictor-gateway/src/db/migrations/1649532710084.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Initial1649532710084 implements MigrationInterface {
+    name = '1649532710084';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+        alter table task alter column offset drop default;
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+    }
+}
