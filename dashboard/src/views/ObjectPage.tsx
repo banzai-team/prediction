47777003d18181babecf5e517aa43f672d832be4
chOlya <Chvyrova.o@mail.ru>
2023-06-25 02:01:53 +0300

    add data

--- dashboard/src/views/ObjectPage.tsx
+++ dashboard/src/views/ObjectPage.tsx
@@ -6,55 +6,24 @@ import PageTitle from "../components/PageTitle";
 import BackButton from "../components/BackButton";
 import {ROUTES} from "./Router";
 import GanttChart from '../components/GanttChart';
-
-import {useQuery,} from 'react-query'
 import {getTasks} from '../domain/api';
+import {useQuery} from 'react-query';
+import Task from '../components/Task';
+import {TaskViewDto} from '../objects/taskDto';
 
 const ObjectPage: React.FC = () => {
     const { id } = useParams();
+    const {data: tasks} = useQuery([id, "tasks"], () => getTasks(id));
+    const tasksForMap: TaskViewDto[] = tasks?.data;
 
-    const { data, isLoading } = useQuery([id, "tasks"], () => getTasks(id));
-    console.log(data, isLoading);
-    
-    const tasks: any[] = [
-        {
-            id: 'Task 1',
-            name: 'Task 1',
-            start: '2016-12-28',
-            end: '2016-12-31',
-            progress: 60,
-            dependencies: 'Task 2'
-        },
-        {
-            id: 'Task 2',
-            name: 'Task 2',
-            start: '2016-11-28',
-            end: '2017-03-03',
-            progress: 90,
-            dependencies: ''
-        },
-        {
-            id: 'Task 3',
-            name: 'Task 3',
-            start: '2016-11-28',
-            end: '2016-12-31',
-            progress: 20,
-            dependencies: 'Task 4'
-        },
-        {
-            id: 'Task 4',
-            name: 'Task 4',
-            start: '2016-11-30',
-            end: '2016-12-15',
-            progress: 55,
-            dependencies: ''
-        },
-    ];
+    const tasksData = tasksForMap && tasksForMap?.length > 0 ? tasksForMap?.map(task => {
+        return Task(task, task.id.toString());
+    }) : [];
 
-    const criticalSum = data?.data.reduce( (a: number, b: any) => {
+    const criticalSum = tasks?.data.reduce( (a: number, b: any) => {
         return b.taskType.isCritical ? a + b.offset : a;
     }, 0);
-
+    
     return (
         <>
             <PageTitle>
@@ -67,9 +36,9 @@ const ObjectPage: React.FC = () => {
                 alignItems="center"
             >
                 <Text>Sum of Critical Offsets:</Text>
-                <Text pl="10px" fontSize="xl" fontWeight="bold">{criticalSum}</Text>
+               <Text pl="10px" fontSize="xl" fontWeight="bold">{criticalSum}</Text>
             </Flex>
-            <GanttChart tasks={tasks}/>
+            <GanttChart tasks={tasksData}/>
         </>
     )
 };
