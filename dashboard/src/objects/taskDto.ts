30914f2f77fdb5ffbd5590e31f72c503620c016c
chOlya <Chvyrova.o@mail.ru>
2023-06-25 00:50:24 +0300

    add objects

---
+++ dashboard/src/objects/taskDto.ts
@@ -0,0 +1,20 @@
+export interface TaskTypeViewDto {
+    code: string;
+    name: string;
+    isCritical: boolean;
+}
+
+export interface TaskHistoryViewDto {
+    id: number;
+    reportDate: Date;
+    actualStart: Date;
+    actualEnd: Date;
+    progress: number;
+}
+
+export interface TaskViewDto {
+    id: number;
+    taskType: TaskTypeViewDto;
+    plannedStart: Date;
+    plannedEnd: Date;
+}
