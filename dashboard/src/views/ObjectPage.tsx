e1e02da3b7295c87d3ca89d8830334a7f5369379
chOlya <Chvyrova.o@mail.ru>
2023-06-24 15:00:02 +0300

    add gantt chart

--- dashboard/src/views/ObjectPage.tsx
+++ dashboard/src/views/ObjectPage.tsx
@@ -4,9 +4,45 @@ import {useParams} from "react-router-dom";
 import PageTitle from "../components/PageTitle";
 import BackButton from "../components/BackButton";
 import {ROUTES} from "./Router";
+import GanttChart from '../components/GanttChart';
 
 const ObjectPage: React.FC = () => {
     const { id } = useParams();
+
+    const tasks: any[] = [
+        {
+            id: 'Task 1',
+            name: 'Task 1',
+            start: '2016-12-28',
+            end: '2016-12-31',
+            progress: 60,
+            dependencies: 'Task 2'
+        },
+        {
+            id: 'Task 2',
+            name: 'Task 2',
+            start: '2016-11-28',
+            end: '2017-03-03',
+            progress: 90,
+            dependencies: ''
+        },
+        {
+            id: 'Task 3',
+            name: 'Task 3',
+            start: '2016-11-28',
+            end: '2016-12-31',
+            progress: 20,
+            dependencies: 'Task 4'
+        },
+        {
+            id: 'Task 4',
+            name: 'Task 4',
+            start: '2016-11-30',
+            end: '2016-12-15',
+            progress: 55,
+            dependencies: ''
+        },
+    ];
 
     return (
         <>
@@ -14,6 +50,7 @@ const ObjectPage: React.FC = () => {
                 Object #{id}
             </PageTitle>
             <BackButton backRoute={ROUTES.DASHBOARD}>Back to dashboard</BackButton>
+            <GanttChart tasks={tasks}/>
         </>
     )
 };
