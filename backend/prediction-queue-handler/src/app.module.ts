bca35a29c9d294cd85c108533901cd2060b5eef7
wuz1mu <kingaal93@gmail.com>
2023-06-25 01:09:46 +0700

    Implement entity update

--- backend/prediction-queue-handler/src/app.module.ts
+++ backend/prediction-queue-handler/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HandlerModule } from './handler/handler.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [HandlerModule],
+  imports: [HandlerModule,
+  
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'building_predictor',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/prediction-queue-handler/src/handler/handler.module.ts
+++ backend/prediction-queue-handler/src/handler/handler.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { HandlerController } from './handler.controller';
+import { HandlerService } from './handler.service';
+import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './handler.entity';
 
 @Module({
-  controllers: [HandlerController]
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([Task])
+  ],
+  controllers: [HandlerController],
+  providers: [HandlerService]
 })
 export class HandlerModule {}
