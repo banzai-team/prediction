a7c2ba2ff6c1cd93561ce38bc12a8acba04793e5
Никита Савченко <nsavchenko@themsteam.com>
2023-06-25 00:52:04 +0300

    fix

--- ml/app/main.py
+++ ml/app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
 
-from src import data_processor, processor, utils
+from . import src
 
 
 app = FastAPI()
@@ -13,10 +13,10 @@ MODEL_FIT_TRANSFORM_PATH = '../models/fit_transform.csv'
 MODEL_CATBOOST_PATH = '../models/predictor'
 
 fit_transform = pd.read_csv(MODEL_FIT_TRANSFORM_PATH, index_col=0, dtype={'task_id': 'str'})
-data_proc = data_processor.DataProcessor()
+data_proc = src.data_processor.DataProcessor()
 X = data_proc.preprocess(fit_transform, add_pca_feats=True, one_hot_encode=True)
 
-pr = processor.Processor(data_proc, MODEL_CATBOOST_PATH)
+pr = src.processor.Processor(data_proc, MODEL_CATBOOST_PATH)
 
 
 class Item(BaseModel):
@@ -35,7 +35,7 @@ def read_root():
 
 @app.post("/predict")
 def read_item(item: Item):
-    sample = utils.DataSample(obj_prg=item.obj_prg,
+    sample = src.utils.DataSample(obj_prg=item.obj_prg,
                               task_id=item.task_id,
                               percent_ready=item.percent_ready,
                               plan_start_date=item.plan_start_date,
