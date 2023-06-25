34e2e242424721de7d3336f99baaa88ff7d14632
chOlya <Chvyrova.o@mail.ru>
2023-06-25 03:56:57 +0300

    remove useQuery

--- dashboard/src/components/Task.tsx
+++ dashboard/src/components/Task.tsx
@@ -2,14 +2,13 @@ import {useQuery} from 'react-query';
 import {getTaskHistory} from '../domain/api';
 
 export function Task(task: any, id: string | undefined) {
-    const {data: taskHistory} = useQuery([id, "taskHistory"], () => getTaskHistory(id));
     
     return {
         id: task?.id,
         name: task?.taskType?.name,
         start: '2016-12-28',
         end: '2016-12-31',
-        progress: taskHistory?.data?.progress,
+        progress: 0,
         dependencies: ''
     };
 }
